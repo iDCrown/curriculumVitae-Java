101cc04021b581dd9f409d3eb8c5402492e99dd1
sara <saradan.vm@gmail.com>
2025-08-10 23:49:20 -0500

    feature:templates del listar y formulario

--- src/main/java/com/vitae/curriculumVitae/formularioController.java
+++ src/main/java/com/vitae/curriculumVitae/formularioController.java
@@ -1,18 +1,26 @@
 package com.vitae.curriculumVitae;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+import org.springframework.ui.Model;
+
+@Controller
 
 //@RequestMapping("/api") este annotation es para definir un prefijo comun en todos los endpoints ejemplo: localhost:8080/api/formulario
 public class formularioController {
-    
     //Endpopint
 
     @GetMapping("/formulario")
-    public String formulario() {
-        return "Formulario de Currículo Vitae";
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("user", new User());
+        return "formulario"; // busca formulario.html en /templates
     }
+
+     @GetMapping("/lista")
+    public String mostrarLista() {
+        return "lista"; 
+    }
+    
 
 }
